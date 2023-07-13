7560acb5d93e5bcb8f0eca7235ab2166e8aede7e
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-13 15:56:20 +0530

    [Prasanna] Add: Completion of Program

--- Day5Problem/Program.cs
+++ Day5Problem/Program.cs
@@ -7,16 +7,22 @@ namespace Day5Problem
         static void Main()
         {
             Console.WriteLine("Solution for the Day Five Problem");
-            //CoinFlip.play();
-            //  CoinFlip.Leap();
-            //  CoinFlip.Powerof2();
-            // CoinFlip.swap();
-            // CoinFlip.OddorEven();
-            //CoinFlip.Largest();
-            //CoinFlip.QuotientRemainder();
-            //CoinFlip.PrimeNumber();
-            // CoinFlip.Vowel();
-            CoinFlip.Harmonics();
+            Console.WriteLine("Write the Program to be Executed");
+            Console.WriteLine("1.CoinFlip\n2.Leap Year\n3.Power Multiple of 2\n4.Swapping\n5.Odd or Even\n6.Largest among 3 Numbers\n7.Quotient and Remainder\n8.Prime Factor\n9. Vowel Or Consonant\n10.Harmonics ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1: CoinFlip.play(); break;
+                case 2: CoinFlip.Leap(); break;
+                case 3: CoinFlip.Powerof2(); break;
+                case 4: CoinFlip.swap(); break;
+                case 5: CoinFlip.OddorEven(); break;
+                case 6: CoinFlip.Largest(); break;
+                case 7: CoinFlip.QuotientRemainder(); break;
+                case 8: CoinFlip.PrimeNumber(); break;
+                case 9: CoinFlip.Vowel(); break;
+                case 10: CoinFlip.Harmonics(); break;
+            }
         }
     }
 }
